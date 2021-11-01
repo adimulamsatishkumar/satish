dc148cbce28f6bc78816972dd1b9ef8b1cdb1728
satish kumar20 <satish.justin747@gmail.com>
2021-11-01 10:36:22 +0530

    vs

---
+++ source/repos/ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("satish!");
+            Console.WriteLine("From Github");
+            Console.WriteLine("From VS");
+
+
+        }
+    }
+}
